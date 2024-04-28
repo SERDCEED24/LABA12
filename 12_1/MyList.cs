b940a175eb9574360cc591fbd06d827423eb637e
George <George@GPC>
2024-04-28 23:37:18 +0500

    Add project files.

---
+++ 12_1/MyList.cs
@@ -0,0 +1,185 @@
+﻿using CarsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _12_1
+{
+    public class MyList<T> where T : IInit, ICloneable, new()
+    {
+        // Поля
+        Point<T>? beg = null;
+        Point<T>? end = null;
+        int count = 0;
+
+        // Свойства
+        public int Count => count;
+
+        // Методы и статические функции
+        public Point<T> GetBeg()
+        {
+            return beg;
+        }
+        public static Point<T> MakeRandomData()
+        {
+            T data = new T();
+            data.RandomInit();
+            return new Point<T>(data);
+        }
+        public static T MakeRandomItem()
+        {
+            T data = new T();
+            data.RandomInit();
+            return data;
+        }
+        public void AddToBegin(T item)
+        {
+            T newData = (T)item.Clone();
+            Point<T> newItem = new Point<T>(newData);
+            count++;
+            if (beg != null)
+            {
+                beg.Prev = newItem;
+                newItem.Next = beg;
+                beg = newItem;
+            }
+            else
+            {
+                beg = newItem;
+                end = beg;
+            }
+        }
+        public void AddToEnd(T item)
+        {
+            T newData = (T)item.Clone();
+            Point<T> newItem = new Point<T>(newData);
+            count++;
+            if (end != null)
+            {
+                end.Next = newItem;
+                newItem.Prev = end;
+                end = newItem;
+            }
+            else
+            {
+                beg = newItem;
+                end = beg;
+            }
+        }
+
+        // Конструкторы
+        public MyList() { }
+        public MyList(int size)
+        {
+            if (size < 0) throw new Exception("Размер списка не может быть меньше 0!");
+            beg = MakeRandomData();
+            end = beg;
+            for (int i = 1; i < size; i++)
+            {
+                T newItem = MakeRandomItem();
+                AddToEnd(newItem);
+            }
+            count = size;
+        }
+        public MyList(T[] collection)
+        {
+            if (collection == null)
+                throw new Exception("Коллекция равна null!");
+            if (collection.Length == 0)
+                throw new Exception("Коллекция пуста!");
+            T newData = (T)collection[0].Clone();
+            beg = new Point<T>(newData);
+            end = beg;
+            for (int i = 1; i < collection.Length; i++)
+            {
+                AddToEnd(collection[i]);
+            }
+        }
+        public void Print()
+        {
+            if (count == 0)
+                Console.WriteLine("Список пустой.");
+            Point<T>? current = beg;
+            for (int i = 0; current != null; i++)
+            {
+                Console.WriteLine(current);
+                current = current.Next;
+            }
+        }
+        public Point<T>? FindItem(T item)
+        {
+            Point<T>? current = beg;
+            while (current != null)
+            {
+                if (current.Data == null)
+                    throw new Exception("Data является null!");
+                if (current.Data.Equals(item))
+                    return current;
+                current = current.Next;
+            }
+            return null;
+        }
+        public bool RemoveItem(T item)
+        {
+            if (beg == null) throw new Exception("Список пуст!");
+            Point<T>? pos = FindItem(item);
+            if (pos == null) return false;
+            count--;
+            // Один элемент
+            if (beg == end)
+            {
+                beg = end = null;
+                return true;
+            }
+            // Первый элемент
+            if (pos.Prev == null)
+            {
+                beg = beg?.Next;
+                beg.Prev = null;
+                return true;
+            }
+            // Последний элемент
+            if (pos.Next == null)
+            {
+                end = end?.Prev;
+                end.Next = null;
+                return true;
+            }
+            // Обычный элемент
+            Point<T> prev = pos.Prev;
+            Point<T> next = pos.Next;
+            pos.Next.Prev = prev;
+            pos.Prev.Next = next;
+            return true;
+        }
+        public void Clear()
+        {
+            beg = end = null;
+            count = 0;
+        }
+        public MyList<T> Clone()
+        {
+            MyList<T> listClone = new MyList<T>();
+            Point<T>? current = beg;
+            T? itemClone;
+            while (current != null)
+            {
+                if (current.Data != null)
+                {
+                    itemClone = (T)current.Data.Clone();
+                }
+                else
+                {
+                    itemClone = default(T);
+                }
+                listClone.AddToEnd(itemClone);
+                current = current.Next;
+            }
+            return listClone;
+        }
+    }
+}
+
---
+++ 12_1/Program.cs
@@ -0,0 +1,145 @@
+﻿using CarsLibrary;
+
+namespace _12_1
+{
+    internal class Program
+    {
+        static MyList<Car> GenerateList()
+        {
+            Console.WriteLine("Введите длину списка:");
+            int size = VHS.Input("Ошибка! Введите целое неотрицательное число!", 0);
+            MyList<Car> list = new MyList<Car>(size);
+            Console.WriteLine("\nСписок сгенерирован.");
+            return list;
+        }
+        static MyList<Car> AddElementByNumber(MyList<Car> list)
+        {
+            Console.WriteLine("Введите номер добавляемого элемента:");
+            int number = VHS.Input("Ошибка! Введите натуральное число не больше длины списка + 1!", 1, list.Count + 1);
+            number--;
+            Car Item = MyList<Car>.MakeRandomItem();
+            if (number == 0)
+            {
+                list.AddToBegin(Item);
+            }
+            else if (number == list.Count)
+            {
+                list.AddToEnd(Item);
+            }
+            else
+            {
+                Point<Car> Data = new Point<Car>(Item);
+                Point<Car>? current = list.GetBeg();
+                for (int i = 0; i <= number; i++)
+                {
+                    if (i == number)
+                    {
+                        current.Prev.Next = Data;
+                        Data.Prev = current.Prev;
+                        Data.Next = current;
+                        current.Prev = Data;
+                    }
+                    current = current.Next;
+                }
+            }
+            Console.WriteLine("\nЭлемент добавлен.");
+            return list;
+        }
+        static MyList<Car> DeleteElementsByData(MyList<Car> list)
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Список пустой.");
+            }
+            else
+            {
+                Console.WriteLine("Введите данные элемента для удаления:");
+                Car car = new Car();
+                car.Init();
+                Point<Car>? Item = list.FindItem(car);
+                if (Item == null)
+                {
+                    Console.WriteLine("\nЭлемент с заданными данными не был найден.");
+                }
+                else
+                {
+                    if (Item.Prev == null)
+                    {
+                        Item.Next = null;
+                        list.Clear();
+                    }
+                    else
+                    {
+                        Item.Prev.Next = null;
+                    }
+                    Console.WriteLine("\nЭлементы были успешно удалены из списка.");
+                }
+            }
+            return list;
+        }
+        static void CloneAndPrint(MyList<Car> list)
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Список пустой.");
+            }
+            else
+            {
+                MyList<Car> clone = list.Clone();
+                Console.WriteLine("\nКлон:");
+                clone.Print();
+                clone.GetBeg().Data.RandomInit();
+                Console.WriteLine("\nИзменённый клон:");
+                clone.Print();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string Menu = "\nВыберите действие с двунаправленным списком:\n" +
+                          "1. Сформировать список.\n" +
+                          "2. Распечатать список.\n" +
+                          "3. Добавить в список элемент с заданным номером.\n" +
+                          "4. Удалить из списка все элементы, начиная с элемента с заданным информационным полем, и до конца списка.\n" +
+                          "5. Выполнить глубокое клонирование списка и распечатать клон.\n" +
+                          "6. Удалить список из памяти.\n" +
+                          "7. Выход.\n";
+            MyList<Car> list = new MyList<Car>();
+            int response;
+            do
+            {
+                Console.WriteLine(Menu);
+                response = VHS.Input("Ошибка! Введите целое число от 1 до 7!", 1, 7);
+                Console.WriteLine();
+                try
+                {
+                    switch (response)
+                    {
+                        case 1:
+                            list = GenerateList();
+                            break;
+                        case 2:
+                            list.Print();
+                            break;
+                        case 3:
+                            list = AddElementByNumber(list);
+                            break;
+                        case 4:
+                            list = DeleteElementsByData(list);
+                            break;
+                        case 5:
+                            CloneAndPrint(list);
+                            break;
+                        case 6:
+                            list.Clear();
+                            Console.WriteLine("Список удалён из памяти.");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            } while (response != 7);
+        }
+    }
+}
