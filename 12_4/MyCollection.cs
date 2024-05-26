67f3192b4d0bd7cc027529140961d8a1bebefa4f
SERDCEED24 <heraklakra@gmail.com>
2024-05-26 18:00:09 +0500

    Added project 12_4. Created MyCollection class and demonstrational program for it.

---
+++ 12_4/MyCollection.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarsLibrary;
+
+namespace _12_4
+{
+    public class MyCollection<T>: MyHashTable<T>, ICollection<T>, IEnumerable<T> where T : IInit, ICloneable, new()
+    {
+        // Конструкторы
+        public MyCollection() : base() { }
+
+        public MyCollection(int length) : base(length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                T item = new T();
+                item.RandomInit(); // Предполагаем, что IInit имеет метод RandomInit
+                AddItem(item);
+            }
+        }
+
+        public MyCollection(MyCollection<T> c) : base(c.Capacity)
+        {
+            foreach (T item in c)
+            {
+                AddItem((T)item.Clone());
+            }
+        }
+
+        // Свойства
+        public bool IsReadOnly => false;
+
+        // Методы
+        public void Add(T item)
+        {
+            AddItem(item);
+        }
+        public void Clear()
+        {
+            for (int i = 0; i < Capacity; i++)
+            {
+                table[i] = default;
+                wasDeleted[i] = false;
+            }
+            count = 0;
+        }
+        public bool Contains(T item)
+        {
+            return base.Contains(item);
+        }
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            if (array.Length - arrayIndex < count)
+                throw new ArgumentException("The destination array has fewer elements than the collection.");
+
+            int j = arrayIndex;
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (table[i] != null)
+                {
+                    array[j++] = table[i];
+                }
+            }
+        }
+        public bool Remove(T item)
+        {
+            return RemoveData(item);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (table[i] != null)
+                {
+                    yield return table[i];
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    /*
+    public class MyEnumerator<T> : IEnumerator<T> where T : IInit, ICloneable, new()
+    {
+        MyCollection<T> _collection;
+        int _currentIndex;
+        T _currentItem;
+
+        public MyEnumerator(MyCollection<T> collection)
+        {
+            _collection = collection;
+            _currentIndex = -1; // Начинаем перед первым элементом
+            _currentItem = default(T);
+        }
+
+        public T Current => _currentItem;
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            while (++_currentIndex < _collection.Capacity)
+            {
+                if (_collection.Table[_currentIndex] != null)
+                {
+                    _currentItem = _collection.Table[_currentIndex];
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            _currentIndex = -1;
+            _currentItem = default(T);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+    */
+}
---
+++ 12_4/Program.cs
@@ -0,0 +1,78 @@
+﻿using CarsLibrary;
+using System.Collections.Immutable;
+namespace _12_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание коллекций разных типов
+            MyCollection<Car> carCollection = new MyCollection<Car>();
+            MyCollection<PassengerCar> passengerCarCollection = new MyCollection<PassengerCar>(5);
+            MyCollection<SUV> suvCollection = new MyCollection<SUV>(3);
+            MyCollection<Truck> truckCollection = new MyCollection<Truck>();
+
+            // Демонстрация добавления элементов
+            Console.WriteLine("Добавление элементов в коллекцию Car:");
+            for (int i = 0; i < 5; i++)
+            {
+                Car car = new Car();
+                car.RandomInit();
+                carCollection.Add(car);
+                Console.WriteLine(car);
+            }
+
+            Console.WriteLine("\nДобавление элементов в коллекцию Truck:");
+            for (int i = 0; i < 3; i++)
+            {
+                Truck truck = new Truck();
+                truck.RandomInit();
+                truckCollection.Add(truck);
+                Console.WriteLine(truck);
+            }
+
+            // Демонстрация метода Contains
+            Console.WriteLine("\nПроверка наличия элемента в коллекции Car:");
+            Car checkCar = carCollection.ToArray()[0];
+            if (checkCar != null)
+            {
+                Console.WriteLine($"Содержит ли коллекция {checkCar}? {carCollection.Contains(checkCar)}");
+            }
+
+            // Демонстрация метода Remove
+            Console.WriteLine("\nУдаление элемента из коллекции Car:");
+            if (checkCar != null)
+            {
+                Console.WriteLine($"Удаление {checkCar}");
+                carCollection.Remove(checkCar);
+            }
+
+            // Демонстрация метода CopyTo
+            Console.WriteLine("\nКопирование элементов коллекции Truck в массив:");
+            Truck[] truckArray = new Truck[truckCollection.Count];
+            truckCollection.CopyTo(truckArray, 0);
+            foreach (Truck truck in truckArray)
+            {
+                Console.WriteLine(truck);
+            }
+
+            // Демонстрация метода Clear
+            Console.WriteLine("\nОчистка коллекции Car:");
+            carCollection.Clear();
+            Console.WriteLine($"Количество элементов в коллекции Car после очистки: {carCollection.Count}");
+
+            // Демонстрация перебора коллекции с использованием foreach
+            Console.WriteLine("\nПеребор коллекции PassengerCar с использованием foreach:");
+            foreach (PassengerCar passengerCar in passengerCarCollection)
+            {
+                Console.WriteLine(passengerCar);
+            }
+
+            Console.WriteLine("\nПеребор коллекции SUV с использованием foreach:");
+            foreach (SUV suv in suvCollection)
+            {
+                Console.WriteLine(suv);
+            }
+        }
+    }
+}
